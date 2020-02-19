b539ce2b2baef1b47c0876239b808879bbaffb84
manel turki <menelturki@gmail.com>
2020-02-19 20:58:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+ static capitalize( string ) {
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
+}
+static sanitize( string ) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+    static titleize( sentence ) {
+    let list = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let words = sentence.split( " " )
+    for ( let n = 0; n < words.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( words[ n ] ) )
+      } else {
+        if ( list.includes( words[ n ] ) ) {
+          result.push( words[ n ] )
+        } else {
+          result.push( this.capitalize( words[ n ] ) )
+        }
+      }
+
+    }
+    return result.join( " " );
+  }
 }
