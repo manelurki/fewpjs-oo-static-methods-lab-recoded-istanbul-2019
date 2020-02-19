446d4400e1d4e400ead9f496621daec4991ed45d
manel turki <menelturki@gmail.com>
2020-02-19 20:56:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ static sanitize( string ) {
     static titleize( sentence ) {
     let list = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
     let result = [];
-    let arrayOfWords = sentence.split( " " )
-    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+    let words = sentence.split( " " )
+    for ( let n = 0; n < words.length; n++ ) {
       if ( n == 0 ) {
-        result.push( this.capitalize( arrayOfWords[ n ] ) )
+        result.push( this.capitalize( words[ n ] ) )
       } else {
-        if ( list.includes( arrayOfWords[ n ] ) ) {
-          result.push( arrayOfWords[ n ] )
+        if ( list.includes( words[ n ] ) ) {
+          result.push( words[ n ] )
         } else {
-          result.push( this.capitalize( arrayOfWords[ n ] ) )
+          result.push( this.capitalize( words[ n ] ) )
         }
       }
 
