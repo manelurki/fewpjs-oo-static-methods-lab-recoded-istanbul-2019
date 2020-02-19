a8c60693e2b8bfd25d305d6716b5c6bc458dd5c3
manel turki <menelturki@gmail.com>
2020-02-19 20:54:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ class Formatter {
     return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
 }
 static sanitize( string ) {
-    return string.replace( /[^A-Za-z0-9 '-]/, '' )
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
   }}
