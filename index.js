fa6f5ba2487751c852f2d7de596f462c2f5bf401
manel turki <menelturki@gmail.com>
2020-02-19 20:53:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Formatter {
  static capitalize( string ) {
     return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
-}}
+}
 static sanitize( string ) {
     return string.replace( /[^A-Za-z0-9 '-]/g, '' )
-  }
+  }}
